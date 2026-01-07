e5acf65e508387d2a6c6eef92a9fb7f3eb3048e3
Martin Weindel <martin.weindel@sap.com>
2026-01-07 15:34:46 +0100

    Add networking-cilium extension

--- pkg/registry/register.go
+++ pkg/registry/register.go
@@ -13,6 +13,7 @@ import (
 	"github.com/gardener/gardener-landscape-kit/pkg/components"
 	"github.com/gardener/gardener-landscape-kit/pkg/components/flux"
 	networking_calico "github.com/gardener/gardener-landscape-kit/pkg/components/gardener-extensions/networking-calico"
+	networking_cilium "github.com/gardener/gardener-landscape-kit/pkg/components/gardener-extensions/networking-cilium"
 	"github.com/gardener/gardener-landscape-kit/pkg/components/gardener/garden"
 	"github.com/gardener/gardener-landscape-kit/pkg/components/gardener/operator"
 	"github.com/gardener/gardener-landscape-kit/pkg/utilities"
@@ -24,6 +25,7 @@ var ComponentList = []func() components.Interface{
 	operator.NewComponent,
 	garden.NewComponent,
 	networking_calico.NewComponent,
+	networking_cilium.NewComponent,
 }
 
 // RegisterAllComponents registers all available components.
